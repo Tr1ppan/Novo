323ccb273acef94f80c98a7a4d182bb86c9aeb12
Tr1ppan <patrik.bergestig@gmail.com>
2025-02-03 21:40:23 +0100

    Add files via upload

---
+++ Uppgift1 1.1.novo/Uppgift.1.1.novo/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace uppettpunktett
+{
+    class program
+    {
+        static void Main()
+        {
+
+            //Välkomsttext
+            Console.WriteLine("Din pensionssimulator i kajjan");    
+            
+            //Fråga användaren efter dess namn.
+            Console.WriteLine("Skriv ditt namn:");
+            
+            //Gör en sträng för att lagra datan användaren har angivit
+            string namn = Console.ReadLine();
+            
+            //Gör så att fältet inte kan lämnas blankt eller att annat än bokstäver används.
+            while (string.IsNullOrWhiteSpace(namn) || namn.Any (C => !char.IsLetter(C)))
+
+            //Ge meddelande till användaren om ogiltig inmatning
+            {
+                Console.Write("Namn kan inte vara tomt eller innehålla siffror. (Skriv t.ex. Nisse) Försök igen: ");
+                namn = Console.ReadLine();
+            }
+            //Fråga efter användarens ålder.
+            Console.WriteLine("Skriv din ålder");
+
+            //Gör en sträng för lagra datan användaren har angivit
+            string ålderInput = Console.ReadLine();
+            
+            //Definera en variabel för hantera siffror
+            int ålder;
+
+            //Definera en variabel för pensionsålder (65)
+            int pensionsålder = 65;
+
+            //Beräkna differens ålder-pensionsålder.
+            //Gör så att användaren inte kan ange negativ siffra / siffra över pensionsålder (65)
+            //Gör så att fältet enbart hanterar siffror
+            while (!int.TryParse(ålderInput, out ålder) || ålder <= 0 || ålder > pensionsålder)
+            
+            //Ge användaren meddelade om ogiltig inmatning
+            { 
+            Console.WriteLine("Ogiltig ålder. Vänligen svar i siffror t.ex. 33");
+                ålderInput = Console.ReadLine();
+            }
+
+            //Skriv ut samald info om användarens namn, ålder och kvar till pension
+            Console.WriteLine($"Ditt namn är {namn} och du är {ålder} år gammal. Du har {pensionsålder - ålder} kvar till pension");
+
+        }
+    }
+}
+
---
+++ Uppgift2 1.1.novo/Uppgift.2.1.1.novo/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+
+namespace Uppgifttvåpunktett
+{
+    class program
+    {
+        static void Main()
+        {
+            //Defineras en klass som gör slumpmässig tal
+            Random random = new Random();
+
+            //Gör en variabel där tal 1-101 slumpas ingår
+            int rättTal = random.Next(1, 101);
+
+            //Gör en variabel som lagrar användarens gissning
+            int gissning = 0;
+
+            //Gör en variabel som lagrar och räknar användarens försök
+            int antalFörsök = 0;
+
+            //Gör en loop tills användaren gissar rätt tal
+            while (gissning != rättTal)
+            {
+
+                Console.WriteLine("Jag tänker på en siffra mellan 1-100, vågar du gissa??");
+                Console.WriteLine("Skriv in ett tal mellan 1-100: ");
+
+                //Lagra användarens svar
+                string inPut = Console.ReadLine();
+
+                //Läser av användarens svar och ger sant eller falskt beroende på svar (siffror eller bokstäver)
+                //Siffror godtas, ej bokstäver.
+                bool nummergissning = int.TryParse(inPut, out gissning);
+
+                //Om användaren gissar fel/skriver bokstäver, be om nytt försök
+                if (!nummergissning)
+                {
+
+                    //Meddela användaren om ogiltigt och be användaren försöka igen
+                    Console.WriteLine("Det är tyvärr fel, försök igen (Psst, endast siffror fungerar): ");
+                    continue; //Hoppa tillbaka till början av loopen, användaren får ny chans
+                }
+                //Ge användaren ny chans efter gissning
+                antalFörsök++;
+
+                //Gör if sats, låg gissning
+                if (gissning < rättTal)
+                {
+                    //Skriv ut för lågt
+
+                    Console.WriteLine("För lågt, försök igen =): ");
+                }
+                //Gör if sats, hög gissning
+                else if (gissning > rättTal)
+                {
+                    //Skriv ut för högt
+
+                    Console.WriteLine("För högt, försök igen =): ");
+                }
+                //Gör en else, rätt
+
+                else
+                //Skriv ut rättsvar text
+                {
+                    Console.WriteLine("Grattis, dig lurar man inte!! RÄTT GISSAT");
+                    Console.WriteLine($"Det tog dig {antalFörsök} försök");
+                }
+            }
+
+        }
+    }
+}
